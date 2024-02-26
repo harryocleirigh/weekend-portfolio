7732840865a5dc6d9f3608427706d25e2bef3175
harryocleirigh <91558668+harryocleirigh@users.noreply.github.com>
2024-02-26 01:08:48 +0000

    Rebuild in vercel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,9 +73,8 @@ const Navbar = () => {
         Harry's Portfolio 👋
         </NavLogo>
         <NavItems>
-          <NavLink href="#About">About</NavLink>
           <NavLink href="#Skills">Skills</NavLink>
-          <NavLink href="#Experience">Experience</NavLink>
+          <NavLink href="#Experience">Work History</NavLink>
           <NavLink href="#Projects">Projects</NavLink>
           <NavLink href="#Education">Education</NavLink>
         </NavItems>
